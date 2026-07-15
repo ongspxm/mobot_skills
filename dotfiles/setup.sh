60a335d6b43d3736584be9588b450674c071472c
ongspxm <ongspxm@gmail.com>
2026-07-15 11:56:47 +0800

    chore: (hooks) set up pre-commit

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+
 cpfile() {
-    if [ "$1" == "$0" ]; then return; fi 
-    ln -s $(pwd)/$1 ~
+    if [ "$1" == "$0" ]; then return; fi
+    rm "$HOME/$1"
+    ln -s "$(pwd)/$1" "$HOME"
 }
 
-for f in *; do cpfile $f; done
-for f in .*; do cpfile $f; done
+for f in *; do cpfile "$f"; done
+for f in .*; do cpfile "$f"; done
 
-grep "source ~/startup.sh" ~/.bashrc || echo "source ~/startup.sh" >> ~/.bashrc
+grep -Fqx "source \$HOME/startup.sh" ~/.bashrc || echo "source \$HOME/startup.sh" >> ~/.bashrc
--- dotfiles/startup.sh
+++ dotfiles/startup.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
+
 set -o vi
 export EDITOR=vim
 alias gitpush="git pull --rebase && git push"
 
+tmux0() {
+    tmux new-session -As "$(basename "$PWD")-$(printf %s "$PWD" | md5sum | cut -c -7)"
+}
+
 # piweb web term works better when tmux is doing the buffering
-[[ $IS_PIWEB == "1" ]] && tmux new-session -As "$(basename $(pwd))-$(echo $(pwd) | md5sum | cut -c -7)"
+[[ $IS_PIWEB == "1" ]] && tmux0
