330d4a6e0502f0ecbe22562953feb18049495c67
ongspxm <ongspxm@gmail.com>
2026-07-15 13:10:19 +0800

    feat: (dotfiles) add worktree helper

--- dotfiles/startup.sh
+++ dotfiles/startup.sh
@@ -2,11 +2,19 @@
 
 set -o vi
 export EDITOR=vim
-alias gitpush="git pull --rebase && git push"
 
+# piweb web term works better when tmux is doing the buffering
 tmux0() {
     tmux new-session -As "$(basename "$PWD")-$(printf %s "$PWD" | md5sum | cut -c -7)"
 }
-
-# piweb web term works better when tmux is doing the buffering
 [[ $IS_PIWEB == "1" ]] && tmux0
+
+# git stuffz
+alias gitpush="git pull --rebase && git push"
+alias gwt="git worktree"
+gwtadd() {
+    gitcommit=${1:-$(git rev-parse --short HEAD)}
+    githome=$(git rev-parse --show-toplevel)
+    gitpath=$(echo "$gitcommit" | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+    gwt add "$githome/.worktree-$gitpath" "$gitcommit"
+}
