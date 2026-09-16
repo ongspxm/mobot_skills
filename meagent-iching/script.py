c96f245ef660d57bf9eacd3a5416afbbf50f9291
ongspxm <ongspxm@gmail.com>
2026-09-16 09:39:47 +0800

    fix: (meagent-iching) use upper-lower hexagram keys

--- meagent-iching/script.py
+++ meagent-iching/script.py
@@ -12,14 +12,15 @@ parser.add_argument("n1", type=int)
 parser.add_argument("n2", type=int)
 args = parser.parse_args()
 
-trigram0 = args.n0 % 8 or 8
-trigram1 = args.n1 % 8 or 8
+upper_trigram = args.n0 % 8 or 8
+lower_trigram = args.n1 % 8 or 8
 moving_line = args.n2 % 6 or 6
-hexagram = f"{trigram0}x{trigram1}"
+hexagram = f"{upper_trigram}x{lower_trigram}"
+key = f"{hexagram}.{moving_line}"
 
 with (Path(__file__).parent / "iching_hexagrams.json").open(encoding="utf-8") as file:
     hexagrams = json.load(file)
 
-print("My base hex is:\n"+hexagrams[hexagram])
-print("\nMy moving line is:\n"+hexagrams[f"{hexagram}.{moving_line}"])
+print(f"{key} My base hex is:\n" + hexagrams[hexagram])
+print(f"\n{key} My moving line is:\n" + hexagrams[key])
 print("\nExplain phrase by phrase then interpret.")
